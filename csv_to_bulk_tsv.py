040ba0b58a072fd206ed5ca01f366b03d09e0dec
William Moore <w.moore@dundee.ac.uk>
2020-01-28 12:40:37 +0000

    csv_to_bulk.tsv ingores png. jpg & mpg only

--- csv_to_bulk_tsv.py
+++ csv_to_bulk_tsv.py
@@ -14,19 +14,14 @@ with open('idr0070-experimentA-filePaths.tsv', mode='w') as tsv_file:
         archive_root = os.path.relpath(root, path_to_data)
         for f in files:
             image_path = os.path.join(root, f)
-            if f.endswith('.png') or f.endswith('.jpg'):
+            if f.endswith('.mpg') or f.endswith('.jpg'):
 
                 dirname = os.path.basename(root)
                 stage = dirname.split('_')[0]
 
                 target = "%sDataset:name:%s-%s" % (project, gene, stage)
 
-                new_name = f
-                if new_name.endswith("_mapped.png"):
-                    # remove _mapped from end and prepend it instead
-                    new_name = new_name.replace("_mapped", "")
-                    new_name = "mapped_%s" % new_name
-                new_name = "%s/%s" % (dirname, new_name)
+                new_name = "%s/%s" % (dirname, f)
 
                 tsv_writer.writerow([target, image_path, new_name])
                 line_count += 1
