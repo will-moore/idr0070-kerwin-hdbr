22ed9e3058a5b40025a51c26ad7a2f298637f2b4
William Moore <w.moore@dundee.ac.uk>
2020-02-03 17:29:15 +0000

    csv_to_bulk_tsv.py reads from csv again

--- csv_to_bulk_tsv.py
+++ csv_to_bulk_tsv.py
@@ -6,24 +6,36 @@ project = "Project:name:idr0070-kerwin-hdbr/experimentA/"
 path_to_data = "/uod/idr/filesets/idr0070-kerwin-hdbr/20191021-original/HDBR_PAX6"
 # path_to_data = "/Users/willadmin/Desktop/IDR/idr0070/data/HDBR_PAX6"
 
-line_count = 0
-with open('idr0070-experimentA-filePaths.tsv', mode='w') as tsv_file:
-    tsv_writer = csv.writer(tsv_file, delimiter='\t')
+with open('HDBR_PAX6.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    line_count = 0
 
-    for root, dirs, files in os.walk(path_to_data):
-        archive_root = os.path.relpath(root, path_to_data)
-        for f in files:
-            image_path = os.path.join(root, f)
-            if f.endswith('.mpg') or f.endswith('.jpg'):
+    with open('idr0070-experimentA-filePaths.tsv', mode='w') as tsv_file:
+        tsv_writer = csv.writer(tsv_file, delimiter='\t')
 
-                dirname = os.path.basename(root)
-                stage = dirname.split('_')[0]
+        for row in csv_reader:
 
-                target = "%sDataset:name:%s-%s" % (project, gene, stage)
-
-                new_name = "%s/%s" % (dirname, f)
-
-                tsv_writer.writerow([target, image_path, new_name])
+            # ignore first row of CSV (column names)
+            if line_count == 0:
                 line_count += 1
+                continue
 
-print("wrote %s lines" % line_count)
+            # Read the columns we need...
+            dir_name = row[0].replace("PAX6_", "")
+            img_name = row[1]
+            stage = row[7]
+
+            image_path = "%s/%s/%s" % (path_to_data, dir_name, img_name)
+
+            # e.g. CS15_IHC_transverse/CS15_N733_64.jpg
+            new_name = "%s/%s" % (dir_name, img_name)
+
+            # check we're creating correct paths
+            if not os.path.exists(image_path):
+                print(new_name)
+                continue
+
+            target = "%sDataset:name:%s-%s" % (project, gene, stage)
+            tsv_writer.writerow([target, image_path, new_name])
+
+            line_count += 1
