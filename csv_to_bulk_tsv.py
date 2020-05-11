ebc9f079f413e3b2d5cca1673406af5522e0ddc3
William Moore <w.moore@dundee.ac.uk>
2020-05-11 15:42:45 +0100

    Fix writing of edited csv

--- csv_to_bulk_tsv.py
+++ csv_to_bulk_tsv.py
@@ -4,15 +4,17 @@ import glob
 
 project = "Project:name:idr0070-kerwin-hdbr/experimentA/"
 path_to_data = "/uod/idr/filesets/idr0070-kerwin-hdbr/"
-# path_to_data = "/Users/wmoore/Desktop/IDR/idr0070/20191021-original/HDBR_PAX6"
 
+# We don't process '20191021-original' first since it has fewer columns
+# and we use the first file to set col_names below.
 batch_dirs = [
-    '20191021-original', '20200414-Batch3-ftp', '20200422-Batch5',
-    '20200214-ftp', '20200417-Batch4', '20200423-Batch6'
+    '20200214-ftp', '20191021-original', '20200414-Batch3-ftp',
+    '20200422-Batch5', '20200417-Batch4', '20200423-Batch6'
 ]
 
 # Read the csv file from each batch, adding to single list of rows.
 csv_rows = []
+col_names = None
 for dir_name in batch_dirs:
     # Find csv file in each dir...
     path_to_csv = os.path.join(os.getcwd(), dir_name)
@@ -24,6 +26,9 @@ for dir_name in batch_dirs:
         csv_reader = csv.DictReader(csv_file)
         rows = list(csv_reader)
         # Need to know dir_name below...
+        cols = list(rows[0].keys())
+        if col_names is None:
+            col_names = cols
         for r in rows:
             # print(r)
             assert r['Dataset Name'] is not None
@@ -40,6 +45,7 @@ for dir_name in batch_dirs:
 # We create the bulk import .tsv and the bulk metadata .csv at the same time...
 with open('idr0070-experimentA-annotation.csv', mode='w') as csv_file:
     csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(col_names)
 
     with open('idr0070-experimentA-filePaths.tsv', mode='w') as tsv_file:
         tsv_writer = csv.writer(tsv_file, delimiter='\t')
@@ -72,8 +78,8 @@ with open('idr0070-experimentA-annotation.csv', mode='w') as csv_file:
             tsv_writer.writerow([target, image_path, new_name])
 
             # Edit rows and write to new csv
-            row[0] = dataset
-            row[1] = new_name
-            csv_writer.writerow(row)
-
-            line_count += 1
+            # convert from dict to list
+            csv_row = [row.get(name, '') for name in col_names]
+            csv_row[0] = dataset
+            csv_row[1] = new_name
+            csv_writer.writerow(csv_row)
