87fbf33add3022b697b68d35cccfe2d1a3e7c9aa
William Moore <w.moore@dundee.ac.uk>
2020-05-19 10:31:15 +0100

    Temp re-purpose validate_imports.py to list MIFs

--- validate_imports.py
+++ validate_imports.py
@@ -41,65 +41,73 @@ datasets = requests.get(url).json()['datasets']
 total_images = 0
 fs_images = 0
 total_filesets = 0
+mi_filesets = 0
 
-if not os.path.exists('imported_image_names.csv'):
+# if not os.path.exists('imported_image_names.csv'):
 
-    with open('imported_image_names.csv', mode='w') as csv_file:
-        csv_writer = csv.writer(csv_file)
+# We want to list all Filesets that have multiple images
+# (not including '[macro]' '[label]' images)
+with open('fileset_images.csv', mode='w') as csv_file:
+    csv_writer = csv.writer(csv_file)
 
-        for d in datasets:
-            # load images
-            print("Dataset", d['id'], d['name'])
-            dataset_id = d['id']
-            img_url = f'http://ome-idr-covid-1.openmicroscopy.org/webclient/api/images/?id={dataset_id}'
-            images = requests.get(img_url).json()['images']
-            filesets = {}
-            total_images += len(images)
-            for i in images:
-                if i['filesetId'] not in filesets:
-                    filesets[i['filesetId']] = []
-                filesets[i['filesetId']].append(i)
-                csv_writer.writerow([i['name']])
-            total_filesets += len(filesets)
-            for fs_imgs in filesets.values():
-                imgs = [i for i in fs_imgs if "[macro" not in i['name']]
-                if len(imgs) > 1:
-                    print([i['name'] for i in imgs])
-                    fs_images += len(imgs)
+    for d in datasets:
+        # load images
+        print("Dataset", d['id'], d['name'])
+        dataset_id = d['id']
+        img_url = f'http://ome-idr-covid-1.openmicroscopy.org/webclient/api/images/?id={dataset_id}'
+        images = requests.get(img_url, timeout=10).json()['images']
+        filesets = {}
+        total_images += len(images)
+        for i in images:
+            if i['filesetId'] not in filesets:
+                filesets[i['filesetId']] = []
+            filesets[i['filesetId']].append(i)
+            # csv_writer.writerow([i['name']])
+        total_filesets += len(filesets)
+        for fs_imgs in filesets.values():
+            imgs = [i for i in fs_imgs if "[macro" not in i['name'] and "[label" not in i['name']]
+            if len(imgs) > 1:
+                mi_filesets += 1
+                for i in imgs:
+                    csv_writer.writerow([i['name']])
+                csv_writer.writerow([''])
+                print([i['name'] for i in imgs])
+                fs_images += len(imgs)
 
-            print('total_filesets', total_filesets)
-            print('total_images', total_images)
-            print('fs_images', fs_images)
+        print('total_filesets', total_filesets)
+        print('total_images', total_images)
+        print('multi-image_filesets', mi_filesets)
+        print('fs_images', fs_images)
 
-print("------------ comparing imported against idr0070-experimentA-filePaths.tsv ----------------")
-not_imorted_count = 0
-to_reimport = []
-with open('imported_image_names.csv', mode='r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    imported_images = [r[0] for r in csv_reader]
+# print("------------ comparing imported against idr0070-experimentA-filePaths.tsv ----------------")
+# not_imorted_count = 0
+# to_reimport = []
+# with open('imported_image_names.csv', mode='r') as csv_file:
+#     csv_reader = csv.reader(csv_file)
+#     imported_images = [r[0] for r in csv_reader]
 
-    with open('idr0070-experimentA-filePaths.tsv', mode='r') as tsv_file:
-        tsv_reader = csv.reader(tsv_file, delimiter='\t')
-        # Find any imported image that matches
-        for row in tsv_reader:
-            image_name = row[2]
-            if image_name.endswith(".mpg"):
-                continue
-            imported = False
-            # print('check', image_name)
-            for i in imported_images:
-                # print(i, image_name in i)
-                if image_name in i:
-                    imported = True
-                    break
-            if not imported:
-                print("NOT imported: ", image_name)
-                not_imorted_count += 1
-                print("not_imorted_count", not_imorted_count)
-                to_reimport.append(row)
+#     with open('idr0070-experimentA-filePaths.tsv', mode='r') as tsv_file:
+#         tsv_reader = csv.reader(tsv_file, delimiter='\t')
+#         # Find any imported image that matches
+#         for row in tsv_reader:
+#             image_name = row[2]
+#             if image_name.endswith(".mpg"):
+#                 continue
+#             imported = False
+#             # print('check', image_name)
+#             for i in imported_images:
+#                 # print(i, image_name in i)
+#                 if image_name in i:
+#                     imported = True
+#                     break
+#             if not imported:
+#                 print("NOT imported: ", image_name)
+#                 not_imorted_count += 1
+#                 print("not_imorted_count", not_imorted_count)
+#                 to_reimport.append(row)
 
-with open('reimport.tsv', mode='w') as tsv_file:
-    tsv_writer = csv.writer(tsv_file, delimiter='\t')
-    tsv_writer.writerows(to_reimport)
+# with open('reimport.tsv', mode='w') as tsv_file:
+#     tsv_writer = csv.writer(tsv_file, delimiter='\t')
+#     tsv_writer.writerows(to_reimport)
 
 
